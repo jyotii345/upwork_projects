20894b6ebbc87f6f1011fa8b225897a068ed7bae
powell <powelltc99@yahoo.com>
2021-05-06 14:29:12 -0400

    added all necessary registration api methods to the api class.

--- lib/classes/aggressor_api.dart
+++ lib/classes/aggressor_api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:core';
 
 import 'package:aggressor_adventures/classes/trip.dart';
 import 'package:flutter/material.dart';
@@ -56,7 +57,8 @@ class AggressorApi {
     return tripList;
   }
 
-  Future<dynamic> getReservationDetails(String reservationId, String contactId) async {
+  Future<dynamic> getReservationDetails(
+      String reservationId, String contactId) async {
     //create and send a reservation view request to the Aggressor Api and return json response
     String url = "https://secure.aggressor.com/api/app/reservations/view/" +
         reservationId +
@@ -70,7 +72,9 @@ class AggressorApi {
     return jsonDecode(await pageResponse.stream.bytesToString());
   }
 
-  Future<dynamic> getInventoryDetails(String reservationId,) async {
+  Future<dynamic> getInventoryDetails(
+    String reservationId,
+  ) async {
     //create and send a inventory view request to the Aggressor Api and return json response
     String url =
         "https://secure.aggressor.com/api/app/reservations/inventory/" +
@@ -95,7 +99,8 @@ class AggressorApi {
     return jsonDecode(await pageResponse.stream.bytesToString());
   }
 
-  Future<dynamic> recordPayment(String reservationId, String amount, String billingContact, String creditType) async {
+  Future<dynamic> recordPayment(String reservationId, String amount,
+      String billingContact, String creditType) async {
     //create and send a payment to be recorded request to the Aggressor Api and return json response
     String url =
         "https://secure.aggressor.com/api/app/payments/record/" + reservationId;
@@ -112,5 +117,104 @@ class AggressorApi {
 
     StreamedResponse pageResponse = await request.send();
     return jsonDecode(await pageResponse.stream.bytesToString());
+  }
+
+  Future<dynamic> sendRegistration(String firstName, String lastName,
+      String email, String password, String dateOfBirth) async {
+    //sends collected information to the API and sees if there are matching users, returns a userID for future queries
+    Response response = await post(
+      Uri.https('secure.aggressor.com', 'api/app/registration/register'),
+      headers: <String, String>{
+        'apikey': apiKey,
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'first': firstName,
+        'last': lastName,
+        'email': email,
+        'password': password,
+        'dob': dateOfBirth,
+      }),
+    );
+
+    return jsonDecode(response.body);
+  }
+
+  Future<dynamic> getSelectContact(String contactId, String userId) async {
+    //allows a user to link user to a contact
+    String url = "https://secure.aggressor.com/api/app/registration/select/" +
+        contactId +
+        "/" +
+        userId;
+
+    Request request = Request("GET", Uri.parse(url))
+      ..headers.addAll({"apikey": apiKey, "Content-Type": "application/json"});
+
+    StreamedResponse pageResponse = await request.send();
+    return jsonDecode(await pageResponse.stream.bytesToString());
+  }
+
+  Future<dynamic> getCountries() async {
+    //returns a list of all countries and their country codes
+    String url = "https://secure.aggressor.com/api/app/registration/countries";
+
+    Request request = Request("GET", Uri.parse(url))
+      ..headers.addAll({"apikey": apiKey, "Content-Type": "application/json"});
+
+    StreamedResponse pageResponse = await request.send();
+    return jsonDecode(await pageResponse.stream.bytesToString());
+  }
+
+  Future<dynamic> getStates() async {
+    //returns a list of all states and their country codes
+    String url = "https://secure.aggressor.com/api/app/registration/states";
+
+    Request request = Request("GET", Uri.parse(url))
+      ..headers.addAll({"apikey": apiKey, "Content-Type": "application/json"});
+
+    StreamedResponse pageResponse = await request.send();
+    return jsonDecode(await pageResponse.stream.bytesToString());
+  }
+
+
+  Future<dynamic> sendNewContact(
+      String userId,
+      String address1,
+      String address2,
+      String city,
+      String state,
+      String province,
+      String country,
+      String zip,
+      String email,
+      String homePhone,
+      String mobilePhone,
+      String dateOfBirth,
+      String gender) async {
+    //creates a new contact and returns success on completion
+    Response response = await post(
+      Uri.https(
+          'secure.aggressor.com', 'api/app/registration/newcontact/' + userId),
+      headers: <String, String>{
+        'apikey': apiKey,
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'address1': address1,
+        'address2': address2,
+        'city': city,
+        'state': state,
+        'province': province,
+        'country': country,
+        'zip': zip,
+        'email': email,
+        'home': homePhone,
+        'mobile': mobilePhone,
+        'dateOfBirth': dateOfBirth,
+        'gender': gender
+      }),
+    );
+
+    return jsonDecode(response.body);
   }
 }
